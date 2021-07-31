cedf9dff31b7badec8fc8c5a23003ad997cc5c9a
[strong1133] <strong1133@naver.com>
2021-07-31 17:19:33 +0900

    CJ대비 JSP 공부 셋팅

---
+++ CJ/jquery_JSP/src/main/java/com/example/jquery_jsp/JqueryJspApplication.java
@@ -0,0 +1,13 @@
+package com.example.jquery_jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JqueryJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JqueryJspApplication.class, args);
+    }
+
+}
---
+++ CJ/jquery_JSP/src/test/java/com/example/jquery_jsp/JqueryJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jquery_jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JqueryJspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
